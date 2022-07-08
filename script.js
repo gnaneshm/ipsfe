fc9199bc3b19cded7b3065bf5b72d026d8f99b06
gnaneshm <gnaneshm95@gmail.com>
2022-07-08 12:14:02 +0530

    chart component in ips

---
+++ script.js
@@ -0,0 +1,422 @@
+
+var data = [
+    {
+        "date":"Nov, 30",
+        "symptoms":{
+            "Morgens": [
+                {
+                    "id": 5,
+                    "patientId": 1,
+                    "dateAndDosage": "0,2ml | 8 Tropfen",
+                    "timeOfDay": "Abends",
+                    "symptomExperiensing": "Schlafstörung",
+                    "severity": 2,
+                    "isTakenCorrectDosage": true,
+                    "medicationToyDosage": "0,2ml | 8 Tropfen",
+                    "useCannabis": true,
+                    "createdAt": "2022-06-13T03:54:12.658Z",
+                    "updatedAt": "2022-06-13T03:54:12.658Z"
+                },
+                {
+                    "id": 5,
+                    "patientId": 1,
+                    "dateAndDosage": "0,2ml | 8 Tropfen",
+                    "timeOfDay": "Abends",
+                    "symptomExperiensing": "Schlafstörung",
+                    "severity": 2,
+                    "isTakenCorrectDosage": true,
+                    "medicationToyDosage": "0,2ml | 8 Tropfen",
+                    "useCannabis": true,
+                    "createdAt": "2022-06-13T03:54:12.658Z",
+                    "updatedAt": "2022-06-13T03:54:12.658Z"
+                },
+                {
+                    "id": 5,
+                    "patientId": 1,
+                    "dateAndDosage": "0,2ml | 8 Tropfen",
+                    "timeOfDay": "Abends",
+                    "symptomExperiensing": "Schlafstörung",
+                    "severity": 2,
+                    "isTakenCorrectDosage": true,
+                    "medicationToyDosage": "0,2ml | 8 Tropfen",
+                    "useCannabis": true,
+                    "createdAt": "2022-06-13T03:54:12.658Z",
+                    "updatedAt": "2022-06-13T03:54:12.658Z"
+                },
+                {
+                    "id": 5,
+                    "patientId": 1,
+                    "dateAndDosage": "0,9ml | 8 Tropfen",
+                    "timeOfDay": "Abends",
+                    "symptomExperiensing": "Schlafstörung",
+                    "severity": 8,
+                    "isTakenCorrectDosage": true,
+                    "medicationToyDosage": "0,2ml | 8 Tropfen",
+                    "useCannabis": true,
+                    "createdAt": "2022-06-13T03:54:12.658Z",
+                    "updatedAt": "2022-06-13T03:54:12.658Z"
+                }
+
+            ],
+            "Mittags": [
+                {
+                    "id": 5,
+                    "patientId": 1,
+                    "dateAndDosage": "0,2ml | 8 Tropfen",
+                    "timeOfDay": "Abends",
+                    "symptomExperiensing": "Schlafstörung",
+                    "severity": 6,
+                    "isTakenCorrectDosage": true,
+                    "medicationToyDosage": "0,2ml | 8 Tropfen",
+                    "useCannabis": true,
+                    "createdAt": "2022-06-13T03:54:12.658Z",
+                    "updatedAt": "2022-06-13T03:54:12.658Z"
+                }
+            ],
+           "Abends":[
+                {
+                    "id": 5,
+                    "patientId": 1,
+                    "dateAndDosage": "0,2ml | 8 Tropfen",
+                    "timeOfDay": "Abends",
+                    "symptomExperiensing": "Schlafstörung",
+                    "severity": 5,
+                    "isTakenCorrectDosage": true,
+                    "medicationToyDosage": "0,2ml | 8 Tropfen",
+                    "useCannabis": true,
+                    "createdAt": "2022-06-13T03:54:12.658Z",
+                    "updatedAt": "2022-06-13T03:54:12.658Z"
+                }
+            ]
+        }
+    },
+    {
+        "date":"Nov, 30",
+        "symptoms":{
+            "Morgens": [
+                {
+                    "id": 5,
+                    "patientId": 1,
+                    "dateAndDosage": "0,2ml | 8 Tropfen",
+                    "timeOfDay": "Abends",
+                    "symptomExperiensing": "Schlafstörung",
+                    "severity": 9,
+                    "isTakenCorrectDosage": true,
+                    "medicationToyDosage": "0,2ml | 8 Tropfen",
+                    "useCannabis": true,
+                    "createdAt": "2022-06-13T03:54:12.658Z",
+                    "updatedAt": "2022-06-13T03:54:12.658Z"
+                }
+            ],
+            "Mittags": [
+                {
+                    "id": 5,
+                    "patientId": 1,
+                    "dateAndDosage": "0,2ml | 8 Tropfen",
+                    "timeOfDay": "Abends",
+                    "symptomExperiensing": "Schlafstörung",
+                    "severity": 6,
+                    "isTakenCorrectDosage": true,
+                    "medicationToyDosage": "0,2ml | 8 Tropfen",
+                    "useCannabis": true,
+                    "createdAt": "2022-06-13T03:54:12.658Z",
+                    "updatedAt": "2022-06-13T03:54:12.658Z"
+                }
+            ],
+           "Abends":[
+                {
+                    "id": 5,
+                    "patientId": 1,
+                    "dateAndDosage": "0,2ml | 8 Tropfen",
+                    "timeOfDay": "Abends",
+                    "symptomExperiensing": "Schlafstörung",
+                    "severity": 5,
+                    "isTakenCorrectDosage": true,
+                    "medicationToyDosage": "0,2ml | 8 Tropfen",
+                    "useCannabis": true,
+                    "createdAt": "2022-06-13T03:54:12.658Z",
+                    "updatedAt": "2022-06-13T03:54:12.658Z"
+                }
+            ]
+        }
+    },
+    {
+        "date":"Nov, 30",
+        "symptoms":{
+            "Morgens": [
+                {
+                    "id": 5,
+                    "patientId": 1,
+                    "dateAndDosage": "0,2ml | 8 Tropfen",
+                    "timeOfDay": "Abends",
+                    "symptomExperiensing": "Schlafstörung",
+                    "severity": 2,
+                    "isTakenCorrectDosage": true,
+                    "medicationToyDosage": "0,2ml | 8 Tropfen",
+                    "useCannabis": true,
+                    "createdAt": "2022-06-13T03:54:12.658Z",
+                    "updatedAt": "2022-06-13T03:54:12.658Z"
+                }
+            ],
+            "Mittags": [
+                {
+                    "id": 5,
+                    "patientId": 1,
+                    "dateAndDosage": "0,2ml | 8 Tropfen",
+                    "timeOfDay": "Abends",
+                    "symptomExperiensing": "Schlafstörung",
+                    "severity": 6,
+                    "isTakenCorrectDosage": true,
+                    "medicationToyDosage": "0,2ml | 8 Tropfen",
+                    "useCannabis": true,
+                    "createdAt": "2022-06-13T03:54:12.658Z",
+                    "updatedAt": "2022-06-13T03:54:12.658Z"
+                }
+            ],
+           "Abends":[
+                {
+                    "id": 5,
+                    "patientId": 1,
+                    "dateAndDosage": "0,2ml | 8 Tropfen",
+                    "timeOfDay": "Abends",
+                    "symptomExperiensing": "Schlafstörung",
+                    "severity": 5,
+                    "isTakenCorrectDosage": true,
+                    "medicationToyDosage": "0,2ml | 8 Tropfen",
+                    "useCannabis": true,
+                    "createdAt": "2022-06-13T03:54:12.658Z",
+                    "updatedAt": "2022-06-13T03:54:12.658Z"
+                }
+            ]
+        }
+    },
+    {
+        "date":"Nov, 30",
+        "symptoms":{
+            "Morgens": [
+                {
+                    "id": 5,
+                    "patientId": 1,
+                    "dateAndDosage": "0,2ml | 8 Tropfen",
+                    "timeOfDay": "Abends",
+                    "symptomExperiensing": "Schlafstörung",
+                    "severity": 2,
+                    "isTakenCorrectDosage": true,
+                    "medicationToyDosage": "0,2ml | 8 Tropfen",
+                    "useCannabis": true,
+                    "createdAt": "2022-06-13T03:54:12.658Z",
+                    "updatedAt": "2022-06-13T03:54:12.658Z"
+                }
+            ],
+            "Mittags": [
+                {
+                    "id": 5,
+                    "patientId": 1,
+                    "dateAndDosage": "0,2ml | 8 Tropfen",
+                    "timeOfDay": "Abends",
+                    "symptomExperiensing": "Schlafstörung",
+                    "severity": 6,
+                    "isTakenCorrectDosage": true,
+                    "medicationToyDosage": "0,2ml | 8 Tropfen",
+                    "useCannabis": true,
+                    "createdAt": "2022-06-13T03:54:12.658Z",
+                    "updatedAt": "2022-06-13T03:54:12.658Z"
+                }
+            ],
+           "Abends":[
+                {
+                    "id": 5,
+                    "patientId": 1,
+                    "dateAndDosage": "0,2ml | 8 Tropfen",
+                    "timeOfDay": "Abends",
+                    "symptomExperiensing": "Schlafstörung",
+                    "severity": 5,
+                    "isTakenCorrectDosage": true,
+                    "medicationToyDosage": "0,2ml | 8 Tropfen",
+                    "useCannabis": true,
+                    "createdAt": "2022-06-13T03:54:12.658Z",
+                    "updatedAt": "2022-06-13T03:54:12.658Z"
+                }
+            ]
+        }
+    },
+    {
+        "date":"Nov, 30",
+        "symptoms":{
+            "Morgens": [
+                {
+                    "id": 5,
+                    "patientId": 1,
+                    "dateAndDosage": "0,2ml | 8 Tropfen",
+                    "timeOfDay": "Abends",
+                    "symptomExperiensing": "Schlafstörung",
+                    "severity": 2,
+                    "isTakenCorrectDosage": true,
+                    "medicationToyDosage": "0,2ml | 8 Tropfen",
+                    "useCannabis": true,
+                    "createdAt": "2022-06-13T03:54:12.658Z",
+                    "updatedAt": "2022-06-13T03:54:12.658Z"
+                }
+            ],
+            "Mittags": [
+                {
+                    "id": 5,
+                    "patientId": 1,
+                    "dateAndDosage": "0,2ml | 8 Tropfen",
+                    "timeOfDay": "Abends",
+                    "symptomExperiensing": "Schlafstörung",
+                    "severity": 6,
+                    "isTakenCorrectDosage": true,
+                    "medicationToyDosage": "0,2ml | 8 Tropfen",
+                    "useCannabis": true,
+                    "createdAt": "2022-06-13T03:54:12.658Z",
+                    "updatedAt": "2022-06-13T03:54:12.658Z"
+                }
+            ],
+           "Abends":[
+                {
+                    "id": 5,
+                    "patientId": 1,
+                    "dateAndDosage": "0,2ml | 8 Tropfen",
+                    "timeOfDay": "Abends",
+                    "symptomExperiensing": "Schlafstörung",
+                    "severity": 5,
+                    "isTakenCorrectDosage": true,
+                    "medicationToyDosage": "0,2ml | 8 Tropfen",
+                    "useCannabis": true,
+                    "createdAt": "2022-06-13T03:54:12.658Z",
+                    "updatedAt": "2022-06-13T03:54:12.658Z"
+                }
+            ]
+        }
+    }
+]
+// for api response, adding one more column to display prescription link
+data.map(item => item["prescription"] = null);
+
+// different glyph image for different timings
+var imagePath = {
+    "Morgens": './assets/Morning.svg',
+    "Mittags": './assets/sun.svg',
+    "Abends": './assets/night.svg'
+}
+
+// get all the headers of main table
+var tableHeaders = [];
+for (var i = 0; i < data.length; i++) {
+    for (var key in data[i]) {
+        if (tableHeaders.indexOf(key) === -1) {
+            tableHeaders.push(key);
+        }
+    }
+}
+
+// required fields from api response for inside Table
+var requiredKeys = ["", "dateAndDosage", "", "symptomExperiensing", "severity"];
+    
+// creating main table 
+var table = document.createElement("table");
+table.className = "no-borders";
+// table.style.width= "100%";
+// table.style.borderCollapse = "collapse";
+
+// TABLE ROW.
+var tr = table.insertRow(-1); 
+
+var textValue;
+// ADD JSON DATA TO THE TABLE AS ROWS.
+for (var i = 0; i < data.length; i++) {
+    tr = table.insertRow(-1);
+    for(var j=0;j<tableHeaders.length; j++){
+        var tabCell = tr.insertCell(-1);
+        if(j==0){
+            if(i == 0){
+                tabCell.className = "outer-elements leftmost-element first-row";
+            }else{
+                tabCell.className = "outer-elements leftmost-element";
+            }
+            tabCell.innerText = data[i][tableHeaders[j]];
+        }
+        if(j==1){
+            // here we are creating inner table and appending it to table cell of outer table.
+            var insideTable = document.createElement("table");
+            insideTable.className = "inner-table";
+            var insideTr = table.insertRow(-1);
+            var routinePresent = Object.keys(data[i]["symptoms"]);
+            for (m=0; m<routinePresent.length; m++){
+                insideTr = insideTable.insertRow(-1);
+                insideTr.style.border = "1px solid #C0C0C0";
+                for(var l=0;l<requiredKeys.length; l++){
+                    var insideTabCell = insideTr.insertCell(-1);
+                    if(l == 0){
+                        var routineDiv = document.createElement('span');
+                        routineDiv.innerText = routinePresent[m];
+                        var imageContainerDiv = document.createElement('span');
+                        var imageDiv = document.createElement('img');
+                        imageDiv.style.marginRight = "10px"
+                        imageDiv.src = imagePath[routinePresent[m]];
+                        imageContainerDiv.appendChild(imageDiv);
+                        insideTabCell.append(imageContainerDiv);
+                        insideTabCell.append(routineDiv);
+                    }else 
+                    if(l == 1){
+                        textValue = data[i]["symptoms"][routinePresent[m]][data[i]["symptoms"][routinePresent[m]].length-1][requiredKeys[l]].split("|");
+                        insideTabCell.innerText = textValue[0];
+                    }else if(l == 2){
+                        insideTabCell.innerText = textValue[1];
+                    }
+                    else if(l == 4){
+                        var severityDiv = document.createElement('div');
+                        severityDiv.className = "bar-component";
+                        severityDiv.innerHTML = data[i]["symptoms"][routinePresent[m]][data[i]["symptoms"][routinePresent[m]].length-1][requiredKeys[l]];
+                        insideTabCell.style.width = "400px";
+                        insideTabCell.append(severityDiv);
+                    }
+                    else{
+                        insideTabCell.innerText = data[i]["symptoms"][routinePresent[m]][data[i]["symptoms"][routinePresent[m]].length-1][requiredKeys[l]];
+                    }
+                }  
+            }
+            tabCell.appendChild(insideTable);
+            
+        }
+        if(j==2){
+            if(i == 0){
+                tabCell.className = "outer-elements rightmost-element first-row";
+            }else{
+                tabCell.className = "outer-elements rightmost-element";
+            }
+            tabCell.innerHTML = "<img src='./assets/slip.svg' />";
+        }
+    }
+}
+// FINALLY ADD THE NEWLY CREATED TABLE WITH JSON DATA TO A CONTAINER.
+var divContainer = document.getElementById("tracker-table");
+divContainer.innerHTML = "";
+divContainer.appendChild(table);
+//}
+
+
+
+// --------------------------------------------------------------------------
+
+
+const barColor = {
+1: "red",
+2: "#00CFB4",
+3: "#52CA9F",
+4: "chartreuse",
+5: "#F2D25E",
+6: "orange",
+7: "yellow",
+8: "dimgray",
+9: "darkturquoise",
+10: "darkcyan",
+}
+const barValue = document.getElementsByClassName("bar-component");
+for(let i=0; i< barValue.length;i++){
+    barValue[i].style.width = `${barValue[i].innerText * 10}%`
+    barValue[i].style.backgroundColor = barValue[i].innerText in barColor && barColor[barValue[i].innerText];
+}
+const button = document.getElementById('prescription-handler');
+button.onclick = ()=> alert('hello world');
